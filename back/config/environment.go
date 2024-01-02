ede7bd3760df995c0ba533c4236ae00972d46e7b
seonwoo0808 <seonwoo0808@pentag.kr>
2024-01-02 21:12:04 +0900

    :sparkles: Added backend basic feature
    connecting DB, logging, load environment variable

---
+++ back/config/environment.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"ssib.al/ssib-al-back/utils/logger"
+)
+
+var (
+	Env *EnvConfig
+)
+
+type EnvConfig struct {
+	DBHost     string // POSTGRES_HOST
+	DBPort     int    // POSTGRES_PORT
+	DBUser     string // POSTGRES_USER
+	DBName     string // POSTGRES_DB
+	DBPassword string // POSTGRES_PASSWORD
+}
+
+func LoadStrEnv(key string) (value string) {
+	value = os.Getenv(key)
+	if value == "" {
+		logger.Panic(fmt.Errorf("env %s is not set", key))
+	}
+	return
+}
+
+func LoadIntEnv(key string) (value int) {
+	strValue := os.Getenv(key)
+	value, err := strconv.Atoi(strValue)
+	if err != nil {
+		logger.Panic(fmt.Errorf("env %s is not set or not int", key))
+	}
+	return
+}
+
+func NewEnvConfig() {
+	Env = &EnvConfig{
+		DBHost:     LoadStrEnv("POSTGRES_HOST"),
+		DBPort:     LoadIntEnv("POSTGRES_PORT"),
+		DBUser:     LoadStrEnv("POSTGRES_USER"),
+		DBName:     LoadStrEnv("POSTGRES_DB"),
+		DBPassword: LoadStrEnv("POSTGRES_PASSWORD"),
+	}
+}
